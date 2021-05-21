c081631b0a7656bd8cccfe8ca9724e7a5619016e
mukit hasan pranto <mukithasanpranto4@gmail.com>
2021-05-21 18:16:27 +0600

    'effect-wrap'

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 $(document).ready(function () {
-    $(".play-video").click(function () { 
-        alert('Hiii');
+    const videoSrc = $('#player-1').attr('src');
+
+    $(".play-video,#close-popup").click(function () { 
+        if ($('.video-popup').hasClass('open-popup')){
+            $('.video-popup').removeClass('open-popup');
+            $('#player-1').attr('src','');
+        }else{
+            $('.video-popup').addClass('open-popup');
+            if ($('#player-1').attr('src')==''){
+                $('#player-1').attr('src',videoSrc);
+            }
+        }
     });
 });
