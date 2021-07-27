5c645868105f4857edef803dbac82b790990fa11
mukit hasan pranto <mukithasanpranto4@gmail.com>
2021-07-27 21:15:55 +0600

    'feature_section'

--- main.js
+++ main.js
@@ -20,4 +20,24 @@ $(document).ready(function () {
             }
         }
     });
+
+    //owl carousel
+
+    $('.feature-carousel').owlCarousel({
+        loop:true,
+        autoplay:true,
+        margin:0,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:2,
+            },
+            1000:{
+                items:3,
+            }
+        }
+    })
 });
